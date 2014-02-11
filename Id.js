30bdde830e92daa1ec7cd87f9afe7846fca8b2fc
Brian Cavalier <brian@hovercraftstudios.com>
2014-02-10 21:07:28 -0500

    Add initial maybeT, uses Id monad as Just

---
+++ Id.js
@@ -0,0 +1,27 @@
+module.exports = Id;
+
+function Id(x) {
+	this._value = x;
+}
+
+Id.of = function(x) {
+	return new Id(x);
+};
+
+Id.prototype.map = function(f) {
+	return this.constructor.of(f(this._value));
+};
+
+Id.prototype.flatMap = function(f) {
+	return f(this._value);
+};
+
+Id.prototype.ap = function(x) {
+	return this.flatMap(function(f) {
+		return x.map(f);
+	});
+};
+
+Id.prototype.toString = Id.prototype.inspect = function() {
+	return 'Just ' + String(this._value);
+};
---
+++ maybe.js
@@ -0,0 +1,70 @@
+var Id = require('./Id');
+var _nothing = Object.create(Id.prototype);
+var just = Id.of;
+
+module.exports = maybe;
+
+function maybe(x) {
+	return isNothing(x) ? nothing() : just(x);
+}
+
+maybe.just = just;
+maybe.nothing = nothing;
+maybe.maybeT = maybeT;
+
+function isNothing(x) {
+	return x === void 0;
+}
+
+function nothing() {
+	return _nothing;
+}
+
+_nothing.map = _nothing.flatMap = _nothing.ap = function() {
+	return this;
+};
+
+_nothing.toString = _nothing.inspect = function() {
+	return 'Nothing';
+};
+
+function maybeT(M) {
+	function MaybeT(x) {
+		this.value = x;
+	}
+
+	MaybeT.of = of;
+
+	function of(x) {
+		return new MaybeT(M.of(just(x)));
+	}
+
+	MaybeT.prototype.flatMap = function(f) {
+		var m = this.value;
+		return new MaybeT(m.flatMap(function(maybe) {
+			return M.of(maybe.flatMap(f));
+		}));
+	};
+
+	MaybeT.prototype.map = function(f) {
+		return this.flatMap(function(x) {
+			return of(f(x));
+		});
+	};
+
+	MaybeT.prototype.ap = function(x) {
+		return this.flatMap(function(f) {
+			return x.map(f);
+		});
+	};
+
+	MaybeT.prototype.toString = function() {
+		return this.value.toString();
+	};
+
+	MaybeT.prototype.inspect = function() {
+		return this.value.inspect();
+	};
+
+	return MaybeT;
+}
