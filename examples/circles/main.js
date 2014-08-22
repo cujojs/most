ce0d0519de455a159f193ecd35f23a8986202bb7
Brian Cavalier <brian@hovercraftstudios.com>
2014-08-22 17:12:07 -0400

    Add flying circles example. See #23

---
+++ examples/circles/main.js
@@ -0,0 +1,90 @@
+var most = require('most');
+
+// Make some circles fly around the window, following the mouse
+// at various delay intervals.
+module.exports = function run() {
+
+	var circle = document.querySelector('.circle');
+	var transformProp = sniffTransformStyleProp(circle);
+	var s = window.getComputedStyle(circle);
+	var w = parseInt(s.width, 10)/2;
+	var h = parseInt(s.height, 10)/2;
+
+	// Number of mouse tails (circles) to create
+	var nTails = 20;
+
+	// Stream mouse movements and turn them into translate3d style rules
+	var mousemoves = most.fromEvent('mousemove', document).map(toTranslate3d);
+
+	// create an infinite stream of delay times: 0,100,200,300,400...etc
+	// We'll limit this with take() later
+	var delays = most.iterate(function (x) { return x + 100; }, 0);
+
+	// Create an infinite stream of random colors
+	var colors = most.iterate(colorGenerator, colorGenerator());
+
+	// For each delay,color pair, create a tail and start translating it
+	// Limit the number of tails to nTails
+	// Merge the flurry of circles into one stream and start observing it
+	// so that it will actually run (streams are lazy: when not observing
+	// then, they are inert!)
+	most.zipWith(makeDelayedTail, delays, colors)
+		.take(nTails)
+		.mergeAll()
+		.forEach(noop);
+
+	// Move a circle by setting its css transform to the provided translate3d
+	function translate(tx, circle) {
+		circle.style[transformProp] = tx;
+		return circle;
+	}
+
+	// Create a tail whose movements will be delayed by the provided delay
+	// and whose color will be the provided color
+	function makeDelayedTail(delay, color) {
+		var tail = makeTail(circle, color);
+		return mousemoves.delay(delay).zipWith(translate, most.repeat(tail));
+	}
+
+	// Turn a mouse event into a css translate3d string
+	function toTranslate3d(e) {
+		return 'translate3d(' + (e.clientX-w) + 'px' + ',' + (e.clientY-h) + 'px' + ',0)';
+	}
+};
+
+// Clone the circle to make a new one and set its color
+function makeTail(circle, color) {
+	var tail = circle.cloneNode(false);
+	tail.style.backgroundColor = color;
+	circle.parentNode.appendChild(tail);
+	return tail;
+}
+
+// Generate a random color
+function colorGenerator() {
+	return 'hsl(' + randomIntBetween(0, 360) + ',' +
+		randomIntBetween(20, 80) + '%,' +
+		randomIntBetween(20, 80) + '%)';
+}
+
+function randomIntBetween(low, high) {
+	return Math.floor(Math.random() * (high - low)) + low;
+}
+
+// Sniff for the particular css transform property we need to use
+function sniffTransformStyleProp (circle) {
+	var transformProp = 'transform';
+	if ('webkitTransform' in circle.style) {
+		transformProp = 'webkitTransform';
+	} else if ('mozTransform' in circle.style) {
+		transformProp = 'mozTransform';
+	} else if ('msTransform' in circle.style) {
+		transformProp = 'msTransform';
+	}
+	return transformProp;
+}
+
+// Log stuff to the console if you want to see the *firehose*
+function noop(x) {
+	//console.log(x);
+}
