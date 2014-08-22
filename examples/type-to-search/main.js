7441f76a643fbdcae667cd084e27c7c047f823af
Brian Cavalier <brian@hovercraftstudios.com>
2014-08-22 16:54:45 -0400

    Add type-to-search wikipedia example. See #23

---
+++ examples/type-to-search/main.js
@@ -0,0 +1,52 @@
+var most = require('most');
+var rest = require('rest/client/jsonp');
+var mime = require('rest/interceptor/mime');
+var entity = require('rest/interceptor/entity');
+
+module.exports = function run() {
+
+	var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+	var fetchResults = rest.wrap(mime).wrap(entity);
+
+	var input = document.getElementById('search');
+	var resultList = document.getElementById('results');
+	var template = document.getElementById('template').innerHTML;
+
+	// Fetch results with rest.js
+	// Returns a promise for the wikipedia json response
+	function getResults(text) {
+		return fetchResults(url + text);
+	}
+
+	// Debounce keystrokes and get input value when it changes
+	// Only search if the user stopped typing for 500ms, and if the
+	// text is longer than 1 character and is different than the last
+	// time we saw the text.
+	var searchText = most.fromEvent('input', input)
+		.debounce(500)
+		.map(function(e) {
+			return e.target.value.trim();
+		})
+		.filter(function(text) {
+			return text.length > 1;
+		})
+		.distinct();
+
+	// Get results from wikipedia API and render
+	// Ignore empty results, extract the actual list of results
+	// from the wikipedia payload, then render the results
+	searchText.map(getResults)
+		.map(most.fromPromise)
+		.switch()
+		.filter(function(response) {
+			return response.length > 1;
+		})
+		.map(function(response) {
+			return response[1];
+		})
+		.observe(function(results) {
+			resultList.innerHTML = results.reduce(function(html, item) {
+				return html + template.replace(/\{name\}/g, item);
+			}, '');
+		});
+};
