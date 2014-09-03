164c738e71c1fef99b4bbcb8ca4ab5c3e929fc9a
Brian Cavalier <brian@hovercraftstudios.com>
2014-09-03 15:06:05 -0400

    Add takeUntil
    Keeps items earlier than a signal from another stream.

--- test/filter-test.js
+++ test/filter-test.js
@@ -3,12 +3,23 @@ var expect = require('buster').expect;
 
 var filter = require('../lib/combinators/filter');
 var build = require('../lib/combinators/build');
+var reduce = require('../lib/combinators/reduce').reduce;
+var observe = require('../lib/combinators/observe').observe;
+var Stream = require('../lib/Stream');
+
 var iterate = build.iterate;
 var repeat = build.repeat;
-var Stream = require('../lib/Stream');
+
+var step = require('../lib/step');
+var Yield = step.Yield;
+var End = step.End;
 
 var sentinel = { value: 'sentinel' };
 var other = { value: 'other' };
+
+function identity(x) {
+	return x;
+}
 
 describe('filter', function() {
 	it('should return a stream containing only allowed items', function() {
@@ -22,12 +33,50 @@ describe('filter', function() {
 	});
 });
 
+describe('takeUntil', function() {
+	it('should only contain events earlier than signal', function() {
+
+		var times = [0, 1, 2, 3, 4, 5];
+		var steps = times.reduceRight(function(step, t) {
+			return new Yield(t, t, step);
+		}, new End(times[times.length-1] + 1));
+
+		var stream = new Stream(identity, steps);
+		var signal = new Stream(identity, new Yield(3));
+
+		return observe(function(x) {
+			expect(x).toBeLessThan(3);
+		}, filter.takeUntil(signal, stream));
+	});
+
+	it('should dispose source stream', function() {
+
+		var times = [0, 1, 2];
+		var steps = times.reduceRight(function(step, t) {
+			return new Yield(t, t, step);
+		}, new End(times[times.length-1] + 1));
+
+		var dispose = this.spy();
+		var stream = new Stream(identity, steps, void 0, dispose);
+		var signal = new Stream(identity, new Yield(1));
+
+		return observe(function() {},
+			filter.takeUntil(signal, stream))
+			.then(function() {
+				expect(dispose).toHaveBeenCalledOnce();
+			});
+
+	});
+});
+
 describe('take', function() {
 	it('should take first n elements', function () {
-		return filter.take(2, repeat(sentinel))
-			.reduce(function (count) {
+		var stream = filter.take(2, repeat(sentinel));
+
+		return reduce(function (count) {
 				return count + 1;
-			}, 0).then(function (count) {
+			}, 0, stream)
+			.then(function (count) {
 				expect(count).toBe(2);
 			});
 	});
@@ -35,15 +84,17 @@ describe('take', function() {
 
 describe('takeWhile', function() {
 	it('should take elements until condition becomes false', function() {
-		return filter.takeWhile(function(x) {
+		var stream = filter.takeWhile(function (x) {
 			return x < 10;
-		}, iterate(function(x) {
+		}, iterate(function (x) {
 			return x + 1;
-		}, 0))
-			.reduce(function(count, x) {
+		}, 0));
+
+		return reduce(function(count, x) {
 				expect(x).toBeLessThan(10);
 				return count + 1;
-			}, 0).then(function(count) {
+			}, 0, stream)
+			.then(function(count) {
 				expect(count).toBe(10);
 			});
 	});
@@ -53,11 +104,12 @@ describe('takeWhile', function() {
 describe('filter', function() {
 
 	it('should return a stream with adjacent duplicates removed', function() {
-		return filter.distinct(Stream.from([1, 2, 2, 3, 4, 4]))
-			.reduce(function(a, x) {
+		var stream = filter.distinct(Stream.from([1, 2, 2, 3, 4, 4]));
+		return reduce(function(a, x) {
 				a.push(x);
 				return a;
-			}, []).then(function(a) {
+			}, [], stream)
+			.then(function(a) {
 				expect(a).toEqual([1,2,3,4]);
 			});
 	});
@@ -68,14 +120,15 @@ describe('distinctBy', function() {
 
 	it('should use provided comparator to remove adjacent duplicates', function() {
 		function eq(a, b) {
-			return a.toLowerCase() === b.toLocaleLowerCase();
+			return a.toLowerCase() === b.toLowerCase();
 		}
 
-		return filter.distinctBy(eq, Stream.from(['a', 'b', 'B', 'c', 'D', 'd']))
-			.reduce(function(a, x) {
+		var stream = filter.distinctBy(eq, Stream.from(['a', 'b', 'B', 'c', 'D', 'd']));
+		return reduce(function(a, x) {
 				a.push(x);
 				return a;
-			}, []).then(function(a) {
+			}, [], stream)
+			.then(function(a) {
 				expect(a).toEqual(['a', 'b', 'c', 'D']);
 			});
 	});
