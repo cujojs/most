a2d7d350f2f9a51a70fc0aed5254e795a18255c0
Brian Cavalier <brian@hovercraftstudios.com>
2014-09-16 09:53:25 -0400

    Add websocket jsdoc

--- websocket.js
+++ websocket.js
@@ -10,6 +10,13 @@ var promise = require('./lib/promises').create;
 exports.fromWebSocket = fromWebSocket;
 exports.toWebSocket = toWebSocket;
 
+/**
+ * Create a stream from a WebSocket.  The stream will end when the WebSocket
+ * closes, and will fail if the WebSocket fails.
+ * @param {WebSocket} ws WebSocket (or compatible, eg SockJS) from
+ *  which to create a stream
+ * @returns {Stream} stream of all the messages received by the WebSocket
+ */
 function fromWebSocket(ws) {
 	return create(function(add, end, error) {
 		pipeFromWebSocket(ws, add, end, error);
@@ -32,6 +39,17 @@ function pipeFromWebSocket(ws, add, end, error) {
 	};
 }
 
+/**
+ * Send all events in a stream to a WebSocket
+ * @param {Stream} stream Stream whose events will be sent to the WebSocket
+ * @param {WebSocket} ws WebSocket (or compatible, eg SockJS) to which to
+ *  send events
+ * @returns {Promise} promise for the end of the stream.  If the WebSocket closes
+ *  before the stream ends, the returned promise will fulfill if the WebSocket
+ *  closes cleanly, or will reject if the WebSocket errors.  If the stream ends
+ *  before the WebSocket closes, the returned promise will fulfill if the stream
+ *  ends cleanly, or will reject if the stream errors.
+ */
 function toWebSocket(stream, ws) {
 	return promise(function(resolve) {
 		pipeToWebSocket(stream, ws, resolve);
