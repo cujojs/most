90ac33622b35849ad9b71ead84d7b8d3db057ca0
Brian Cavalier <brian@hovercraftstudios.com>
2014-09-23 13:44:38 -0400

    Simplify zip and merge apis to a single variant each

--- test/zip-test.js
+++ test/zip-test.js
@@ -1,57 +1,33 @@
 require('buster').spec.expose();
 var expect = require('buster').expect;
 
-var zip = require('../lib/combinators/zip');
+var zip = require('../lib/combinators/zip').zip;
+var reduce = require('../lib/combinators/reduce').reduce;
 var Stream = require('../lib/Stream');
 
-describe('zipWith', function() {
-	it('should invoke f for each tuple', function() {
-		var spy = this.spy();
-		var a = [1,2,3];
-		var b = [4,5,6];
-		return zip.zipWith(spy, Stream.from(a), Stream.from(b))
-			.reduce(function(i) {
-				expect(spy).toHaveBeenCalledWith(a[i], b[i]);
-				return i + 1;
-			}, 0);
-	});
-});
-
 describe('zip', function() {
-	it('should yield zipped tuples', function() {
-		var a = [1,2,3];
-		var b = [4,5,6];
-		var i = 0;
-		return zip.zip(Stream.from(a), Stream.from(b))
-			.reduce(function(i, zipped) {
-				expect(zipped).toEqual([a[i], b[i]]);
-				return i + 1;
-			}, 0);
-	});
-});
-
-describe('zipArrayWith', function() {
 	it('should invoke f for each tuple', function() {
 		var spy = this.spy();
 		var a = [1,2,3];
 		var b = [4,5,6];
-		return zip.zipArrayWith(spy, [Stream.from(a), Stream.from(b)])
-			.reduce(function(i) {
+		var s = zip(spy, Stream.from(a), Stream.from(b));
+
+		return reduce(function(i) {
 				expect(spy).toHaveBeenCalledWith(a[i], b[i]);
 				return i + 1;
-			}, 0);
+			}, 0, s);
 	});
 
 	it('should end when shortest stream ends', function() {
 		var spy = this.spy();
 		var a = [1,2];
 		var b = [4,5,6];
-		return zip.zipArrayWith(spy, [Stream.from(a), Stream.from(b)])
-			.reduce(function(count) {
+		var s = zip(spy, Stream.from(a), Stream.from(b));
+
+		return reduce(function(count) {
 				return count + 1;
-			}, 0).then(function(count) {
+			}, 0, s).then(function(count) {
 				expect(count).toBe(a.length);
 			});
-
 	});
 });
