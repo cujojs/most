b5293ed31f3d17af2479944fe19e5081059b8cbb
Brian Cavalier <brian@hovercraftstudios.com>
2014-12-07 15:00:48 -0500

    Add zip

--- test/zip-test.js
+++ test/zip-test.js
@@ -1,16 +1,19 @@
 require('buster').spec.expose();
 var expect = require('buster').expect;
 
-var zip = require('../lib/combinators/zip').zip;
-var reduce = require('../lib/combinators/reduce').reduce;
-var Stream = require('../lib/Stream');
+var iterate = require('../lib/combinator/build').iterate;
+var take = require('../lib/combinator/slice').take;
+var zip = require('../lib/combinator/zip').zip;
+var delay = require('../lib/combinator/delay').delay;
+var reduce = require('../lib/combinator/accumulate').reduce;
+var fromArray = require('../lib/source/fromArray').fromArray;
 
 describe('zip', function() {
 	it('should invoke f for each tuple', function() {
 		var spy = this.spy();
 		var a = [1,2,3];
 		var b = [4,5,6];
-		var s = zip(spy, Stream.from(a), Stream.from(b));
+		var s = zip(spy, delay(0, fromArray(a)), delay(0, fromArray(b)));
 
 		return reduce(function(i) {
 				expect(spy).toHaveBeenCalledWith(a[i], b[i]);
@@ -19,15 +22,21 @@ describe('zip', function() {
 	});
 
 	it('should end when shortest stream ends', function() {
-		var spy = this.spy();
-		var a = [1,2];
-		var b = [4,5,6];
-		var s = zip(spy, Stream.from(a), Stream.from(b));
+		var spy = function(a, b) {
+			return a+b;
+		};
 
-		return reduce(function(count) {
-				return count + 1;
-			}, 0, s).then(function(count) {
-				expect(count).toBe(a.length);
-			});
+		var a = take(2, iterate(inc, 1));
+		var b = take(3, iterate(inc, 4));
+
+		var s = zip(spy, a, b);
+
+		return reduce(inc, 0, s).then(function(count) {
+			expect(count).toBe(2);
+		});
 	});
 });
+
+function inc(x) {
+	return x+1;
+}
