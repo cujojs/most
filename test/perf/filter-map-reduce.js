5a4cc96d74e92665d03739eb82cf440f2576ec2e
Brian Cavalier <brian@hovercraftstudios.com>
2014-12-09 06:44:36 -0500

    Add initial perf tests

---
+++ test/perf/filter-map-reduce.js
@@ -0,0 +1,74 @@
+var most = require('../../most');
+var rx = require('rx');
+var bacon = require('baconjs');
+var kefir = require('kefir');
+var lodash = require('lodash');
+var highland = require('highland');
+var Promise = require('when/lib/Promise');
+
+var runner = require('./runner');
+
+var kefirFromArray = runner.kefirFromArray;
+
+// Create a stream from an Array of n integers
+// filter out odds, map remaining evens by adding 1, then reduce by summing
+var n = 1000000;
+var a = new Array(n);
+
+for(var i = 0; i< a.length; ++i) {
+	a[i] = i;
+}
+
+runner.run({
+	most: runMost,
+	lodash: runLodash,
+	array: runArray,
+	kefir: runKefir,
+	highland: runHighland,
+	rx: runRx,
+	bacon: runBacon
+}, 'filter -> map -> reduce ' + n + ' integers', a).then(function() {
+	console.log('DONE');
+});
+
+function runArray(a) {
+	return a.filter(even).map(add1).reduce(sum, 0);
+}
+
+function runLodash(a) {
+	return lodash(a).filter(even).map(add1).reduce(sum, 0);
+}
+
+function runHighland(a) {
+	return highland(a).filter(even).map(add1).reduce(0, sum);
+}
+
+function runMost(a) {
+	return most.from(a).filter(even).map(add1).reduce(sum, 0);
+}
+
+function runRx(a) {
+	return rx.Observable.fromArray(a).filter(even).map(add1).reduce(sum, 0);
+}
+
+function runBacon(a) {
+	return bacon.fromArray(a).filter(even).map(add1).reduce(0, sum);
+}
+
+function runKefir(a) {
+	return kefirFromArray(a).filter(even).map(add1).reduce(sum, 0);
+}
+
+function sum(x, y) {
+	return x + y;
+}
+
+function add1(x) {
+	return x + 1;
+}
+
+function even(x) {
+	return x % 2 === 0;
+}
+
+function noop() {}
---
+++ test/perf/zip.js
@@ -0,0 +1,73 @@
+var most = require('../../most');
+var rx = require('rx');
+var kefir = require('kefir');
+var bacon = require('baconjs');
+var lodash = require('lodash');
+var highland = require('highland');
+var Promise = require('when/lib/Promise');
+
+var runner = require('./runner');
+
+var kefirFromArray = runner.kefirFromArray;
+
+// Create 2 streams, each with n items, zip them by summing the
+// corresponding index pairs, then reduce the resulting stream by summing
+var n = 100000;
+var a = new Array(n);
+var b = new Array(n);
+
+for(var i = 0; i<n; ++i) {
+	a[i] = i;
+	b[i] = i;
+}
+
+runner.run({
+	most: runMost,
+	lodash: runLodash,
+	kefir: runKefir,
+	highland: runHighland,
+	rx: runRx,
+	bacon: runBacon
+}, 'zip ' + n + ' x 2', { a:a, b:b }).then(function() {
+	console.log('DONE');
+});
+
+function runMost(arrays) {
+	return most.from(arrays.a).zip(add, most.from(arrays.b)).reduce(add, 0);
+}
+
+function runLodash(arrays) {
+	var pairs = lodash.zip(arrays.a, arrays.b);
+	var added = lodash.map(pairs, addPair);
+	return lodash.reduce(added, add, 0);
+}
+
+function addPair(pair) {
+	return pair[0] + pair[1];
+}
+
+function runHighland(arrays) {
+	return highland(arrays.a).zip(highland(arrays.b)).map(addPair).reduce(0, add);
+}
+
+function runRx(arrays) {
+	return rx.Observable.fromArray(arrays.a).zip(rx.Observable.fromArray(arrays.b), add).reduce(add, 0);
+}
+
+function runBacon(arrays) {
+	return bacon.zipWith(add, bacon.fromArray(arrays.a), bacon.fromArray(arrays.b)).reduce(0, add);
+}
+
+function runKefir(arrays) {
+	return kefirFromArray(arrays.a).zip(kefirFromArray(arrays.b), add).reduce(add, 0);
+}
+
+function add(a, b) {
+	return a + b;
+}
+
+function noop() {}
+
+function inc(x) {
+	return x+1;
+}
