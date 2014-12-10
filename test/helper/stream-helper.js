6451d685aee62c91f45e45d59e8daae3eaee3034
Brian Cavalier <brian@hovercraftstudios.com>
2014-12-10 10:56:54 -0500

    Port lift test, improve flatMapEnd dispose
    Remove unused bits from stream-helper

--- test/helper/stream-helper.js
+++ test/helper/stream-helper.js
@@ -1,22 +1,9 @@
 var expect = require('buster').expect;
-//var Stream = require('../../lib/Stream');
-//var step = require('../../lib/step');
-//var sync = require('../../lib/combinators/timed').sync;
+
 var observe = require('../../lib/combinator/observe').observe;
-//var Scheduler = require('../../lib/Scheduler');
 var Promise = require('../../lib/Promise');
-//var promise = require('../../lib/promises');
-
-//var PromiseImpl = promise.Promise;
-//var resolve = promise.Promise.resolve;
-
-//var Yield = step.Yield;
-//var End = step.End;
 
 exports.assertSame = assertSame;
-//exports.makeStepsFromTimes = makeStepsFromTimes;
-//exports.makeStreamFromTimes = makeStreamFromTimes;
-//exports.createTestScheduler = createTestScheduler;
 
 function assertSame(p1, p2) {
 	return new Promise(function(resolve, reject) {
@@ -28,66 +15,3 @@ function assertSame(p1, p2) {
 		}, p1);
 	});
 }
-
-//function makeStreamFromTimes(times, endTime, scheduler) {
-//	return sync(new Stream(identity, makeStepsFromTimes(times, endTime), scheduler));
-//}
-//
-//function makeStepsFromTimes(times, endTime) {
-//	return times.reduceRight(function(s, t) {
-//		return new Yield(t, t, s);
-//	}, new End(endTime));
-//}
-
-/**
- * @returns {Scheduler} a scheduler whose clock is externally controllable
- *  via its tick(ticks:Number) method.
- */
-//function createTestScheduler() {
-//	var s = new Scheduler(setTimer, clearTimer, now, thrower);
-//
-//	s._now = 0;
-//
-//	s.tick = function(ticks, step) {
-//		if(typeof step !== 'number') {
-//			step = ticks;
-//		}
-//
-//		var s = this;
-//		var p = resolve();
-//		for(var i=0; i<ticks; ++i) {
-//			p = p.then(tickStep);
-//		}
-//
-//		return p;
-//
-//		function tickStep() {
-//			return tick(s, step);
-//		}
-//	};
-//
-//
-//	return s;
-//
-//	function setTimer() {}
-//	function clearTimer() {}
-//	function now() { return this._now; }
-//}
-//
-//function tick(s, step) {
-//	return new promise.Promise(function(resolve) {
-//		setTimeout(function() {
-//			s._now += step;
-//			s._runReadyTasks();
-//			resolve(s.now());
-//		}, 0);
-//	});
-//}
-//
-//function thrower(e) {
-//	throw e;
-//}
-//
-//function identity(x) {
-//	return x;
-//}
