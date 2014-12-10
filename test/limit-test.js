dbcc7fa6a059c0f677288605ca9b65db8c0297f2
Brian Cavalier <brian@hovercraftstudios.com>
2014-12-10 12:53:05 -0500

    Port throttle tests

--- test/limit-test.js
+++ test/limit-test.js
@@ -2,6 +2,9 @@ require('buster').spec.expose();
 var expect = require('buster').expect;
 
 var limit = require('../lib/combinator/limit');
+var periodic = require('../lib/source/periodic').periodic;
+var map = require('../lib/combinator/transform').map;
+var take = require('../lib/combinator/slice').take;
 var reduce = require('../lib/combinator/accumulate').reduce;
 var observe = require('../lib/combinator/observe').observe;
 var fromArray = require('../lib/source/fromArray').fromArray;
@@ -66,40 +69,21 @@ describe('//debounce', function() {
 	});
 });
 
-describe('//throttle', function() {
+describe('throttle', function() {
 	it('should exclude items that are too frequent', function() {
-		var scheduler = createTestScheduler();
 
-		var s = timed.throttleOn(scheduler, 1, take(5, timed.periodicOn(scheduler, 1)));
+		var i = 0;
+		var s = take(10, map(function() {
+			return i++;
+		}, periodic(1)));
 
-		var result = reduce(function(a, x) {
+		var throttled = limit.throttle(1, s);
+
+		return reduce(function(a, x) {
 			return a.concat(x);
-		}, [], s)
+		}, [], throttled)
 			.then(function(x) {
-				expect(x).toEqual([0,2,4]);
+				expect(x).toEqual([0,2,4,6,8]);
 			});
-
-		scheduler.tick(10, 1);
-		return result;
-	});
-
-	it('should include items beyond throttle window', function() {
-		var scheduler = createTestScheduler();
-
-		var s = Stream.from([
-			new Stream(identity, new Yield(0, 'a', new End(0)), scheduler),
-			new Stream(identity, new Yield(1, 'z', new End(1)), scheduler),
-			new Stream(identity, new Yield(2, 'b', new End(2)), scheduler)
-		]);
-
-		var result = reduce(function(r, x) {
-			return r+x;
-		}, '', timed.throttle(1, flatMap(identity, s)))
-			.then(function(r) {
-				expect(r).toBe('ab');
-			});
-
-		scheduler.tick(10, 1);
-		return result;
 	});
 });
