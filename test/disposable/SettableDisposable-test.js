064425a9ac5486a483bf84357917e621fb0b1926
Brian Cavalier <brian@hovercraftstudios.com>
2016-01-27 16:14:23 -0500

    Safely dispose in the face of exceptions.
    Rejected promises were handled, but exceptions could have caused a
    leak.
    
    Add unit test coverage for disposables.

---
+++ test/disposable/SettableDisposable-test.js
@@ -0,0 +1,44 @@
+require('buster').spec.expose();
+var expect = require('buster').expect;
+
+var SettableDisposable = require('../../lib/disposable/SettableDisposable');
+
+describe('SettableDisposable', function() {
+	it('should allow setDisposable before dispose', function() {
+		var d = new SettableDisposable();
+		var data = {};
+
+		d.setDisposable({
+			dispose: function() { return data; }
+		});
+
+		var x = d.dispose();
+
+		expect(x).toBe(data);
+	});
+
+	it('should allow setDisposable after dispose', function() {
+		var d = new SettableDisposable();
+		var data = {};
+
+		var p = d.dispose();
+
+		d.setDisposable({
+			dispose: function() { return data; }
+		});
+
+		return p.then(function(x) {
+			expect(x).toBe(data);
+		});
+	});
+
+	it('should allow setDisposable at most once', function() {
+		var d = new SettableDisposable();
+
+		d.setDisposable({});
+
+		expect(function() {
+			d.setDisposable({});
+		}).toThrow();
+	});
+});
