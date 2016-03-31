aa2c71b400aeaad88d3ef60b57486f75b9b1a1e2
Brian Cavalier <brian@hovercraftstudios.com>
2016-03-31 08:37:26 -0400

    Bump version, rebuild

--- dist/most.js
+++ dist/most.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define(factory);
+		define([], factory);
 	else if(typeof exports === 'object')
 		exports["most"] = factory();
 	else
