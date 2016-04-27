1f984fe3ee8a4756e13695a0cc0ac41e71a1db01
Brian Cavalier <brian@briancavalier.com>
2016-04-27 12:07:01 -0400

    Simplify slice (#242)
    * Simplify slice fusion, add experimental map-slice commutation
    
    * Counting down and comparing to zero turns out to be slightly faster

--- test/helper/stream-helper.js
+++ test/helper/stream-helper.js
@@ -3,9 +3,16 @@ var expect = require('buster').expect;
 var reduce = require('../../lib/combinator/accumulate').reduce;
 
 exports.assertSame = assertSame;
+exports.expectArray = expectArray;
 
 function assertSame(s1, s2) {
 	return Promise.all([toArray(s1), toArray(s2)]).then(arrayEquals);
+}
+
+function expectArray(array, s) {
+	return toArray(s).then(function(result) {
+		expect(result).toEqual(array);
+	});
 }
 
 function toArray(s) {
