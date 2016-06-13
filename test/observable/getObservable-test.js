4ce54a73a1c892ba7e291910d19ddcfa079e5d5c
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2016-06-13 11:43:12 -0700

    allow anything that implements Observable

--- test/observable/getObservable-test.js
+++ test/observable/getObservable-test.js
@@ -8,13 +8,15 @@ describe('getObservable', function() {
 	it('should return null for non-object', function() {
 		assert.same(null, getObservable(1));
 		assert.same(null, getObservable(undefined));
-		assert.same(null, getObservable(''));
+		assert.same(null, getObservable(null));
+		assert.same(null, getObservable('string'));
 		assert.same(null, getObservable(true));
 	});
 
 	it('should return null for non-observable object', function() {
 		assert.same(null, getObservable({}));
 		assert.same(null, getObservable(null));
+		assert.same(null, getObservable(() => {}));
 	});
 
 	it('should throw TypeError for invalid observable', function() {
@@ -42,7 +44,7 @@ describe('getObservable', function() {
 
 	it('should return observable if valid', function() {
 		var outer = {};
-		var inner = {};
+		var inner = { subscribe: () => {} };
 		outer[symbolObservable] = function() {
 			return inner
 		}
