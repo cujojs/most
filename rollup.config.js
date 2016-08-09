5235804ce9c74a61f30d1bfcfd4df33a82f63d94
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-09 14:32:01 -0400

    chore(rollup): update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  entry: 'most.js',
+  entry: 'src/index.js',
   dest: 'dist/most.js',
   format: 'umd',
   moduleName: 'most',
