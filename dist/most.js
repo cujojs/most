cbcf9c05654622de558ba3e49b2993203a1be5e6
Brian Cavalier <brian@hovercraftstudios.com>
2016-08-14 21:02:46 -0400

    Bump version, rebuild

--- dist/most.js
+++ dist/most.js
@@ -4399,7 +4399,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	Switch.prototype.run = function(sink, scheduler) {
 		var switchSink = new SwitchSink(sink, scheduler);
-		return dispose.all(switchSink, this.source.run(switchSink, scheduler));
+		return dispose.all([switchSink, this.source.run(switchSink, scheduler)]);
 	};
 
 	function SwitchSink(sink, scheduler) {
@@ -4426,7 +4426,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	};
 
 	SwitchSink.prototype.dispose = function() {
-		return this._disposeCurrent(0);
+		return this._disposeCurrent(this.scheduler.now());
 	};
 
 	SwitchSink.prototype._disposeCurrent = function(t) {
