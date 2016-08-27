b40cec12d016eb54ed943f846457ff5f270177c7
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-27 00:39:27 -0400

    chore(rollup): all dependencies provide ES6 builds
    No longer need commonjs plugin since all dependencies provide es6 builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import buble from 'rollup-plugin-buble'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/index.js',
@@ -11,34 +10,7 @@ export default {
   plugins: [
     buble(),
     nodeResolve({
-      jsnext: true 
-    }),
-    commonjs({
-      exclude: ['node_modules/symbol-observable/**'],
-      // Can not resolve '*' type exports without guidance
-      namedExports: {
-        '@most/prelude': [
-          // array.js
-          'cons',
-          'append',
-          'drop',
-          'tail',
-          'copy',
-          'map',
-          'reduce',
-          'replace',
-          'remove',
-          'removeAll',
-          'findIndex',
-          'isArrayLike',
-          // function.js
-          'id',
-          'compose',
-          'apply',
-          'curry2',
-          'curry3'
-        ]
-      }
+      jsnext: true
     })
   ]
 }
