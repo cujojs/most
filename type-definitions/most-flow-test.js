41595c06f2f3b2b3d7d57e6f3717d1355956cfea
Bogdan Chadkin <trysound@yandex.ru>
2017-08-30 01:35:29 +0300

    Allow omitting properties of subscriber

---
+++ type-definitions/most-flow-test.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { just, subscribe } from '../src'
+
+subscribe({}, just(1))
+subscribe({ next: d => { (d: number) } }, just(1))
+subscribe({ error: err => { (err: Error) } }, just(1))
+subscribe({ complete: () => {} }, just(1))
+const { unsubscribe } = subscribe({
+  next: () => {},
+  error: () => {},
+  complete: () => {}
+}, just(1))
+unsubscribe()
+// $ExpectError
+subscribe()
+// $ExpectError
+subscribe({ next: d => { (d: string) } }, just(1))
+// $ExpectError
+subscribe({ error: err => { (err: string) } }, just(1))
+// $ExpectError
+unsubscribe(1)
