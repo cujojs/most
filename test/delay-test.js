ca759b615ccf033e6cc6b77e11429b8b8abe5bab
Brian Cavalier <brian@briancavalier.com>
2017-09-14 22:15:35 -0400

    fix(delay): Add regression test

--- test/delay-test.js
+++ test/delay-test.js
@@ -3,7 +3,13 @@ const { describe, it } = spec
 const { assert } = referee
 
 import { delay } from '../src/combinator/delay'
-import { collectEvents, makeEvents, ticks } from './helper/testEnv'
+import { map, tap } from '../src/combinator/transform'
+import { switchLatest } from '../src/combinator/switch'
+import { continueWith } from '../src/combinator/continueWith'
+import { take } from '../src/combinator/slice'
+import { of as just } from '../src/source/core'
+import { periodic } from '../src/source/periodic'
+import { collectEvents, makeEvents, ticks, at } from './helper/testEnv'
 
 describe('delay', () => {
   it('should delay events by delayTime', () => {
@@ -15,5 +21,17 @@ describe('delay', () => {
         { time: 1, value: 0 },
         { time: 2, value: 1 }
       ]))
+  })
+
+  it('should dispose pending tasks', () => {
+    // Test derived from example:
+    // https://github.com/cujojs/most/issues/488
+    let count = 0
+    const toDelay = x => tap(() => count++, delay(10, just(x)))
+    const s0 = switchLatest(map(toDelay, take(2, periodic(1, 1))))
+    const s1 = continueWith(() => at(100, 0), s0)
+
+    return collectEvents(s1, ticks(30)).then(() =>
+      assert.equals(1, count))
   })
 })
